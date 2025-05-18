23a29ee9c231b9a06bcd38d347ab5635299c7c91
Mohammed Shabaaz <mohammedshabaaz0199@gmail.com>
2025-05-18 19:16:23 +0530

    Add Day 6 Project

---
+++ Day006_Maze_Solver (Reeborg’s World)/README.md
@@ -0,0 +1,15 @@
+# 🧭 Day 6 - Maze Solver (Reeborg's World)
+
+Solves a maze using basic movement and decision logic in Reeborg’s World.
+
+## 🧠 Concepts Practiced
+
+- Loops
+- `if/else` Logic
+- Custom Functions
+
+## 💻 Example Output
+
+```bash
+(Reeborg moves autonomously through the maze until it reaches the goal)
+```
---
+++ Day006_Maze_Solver (Reeborg’s World)/main.py
@@ -0,0 +1,23 @@
+# Reeborg's World - Maze Solver
+# This code is designed to navigate a maze in Reeborg's World.
+
+"""def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        while front_is_clear():
+            move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()"""
+        
